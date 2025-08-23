8112180c61b4ad52841d1166f17b5409d31111cb
notbilldavis <wdavis06@gmail.com>
2025-08-23 00:45:12 -0400

    better error handling, just show the files you are missing

--- lua/updatehelper.lua
+++ lua/updatehelper.lua
@@ -1,56 +1,36 @@
 local https = require("ssl.https")
 local ltn12 = require("ltn12")
 
-local UPD = {}
+local updateAndRequire, update, update_legacy, check_file
 
-function UPD.Update(files)
+local github_url = "https://raw.githubusercontent.com/notbilldavis/mm-mush-plugins/refs/heads/main/"
+
+updateAndRequire = function(local_path, file_name)
+  if not local_path or not file_name then
+    return false, nil
+  end
+
+  local success, result = pcall(check_file, local_path .. file_name, github_url .. file_name)
+  if not success then
+    return false, nil
+  end
+  
+  return pcall(require, file_name:gsub("%.lua$", ""))
+end
+
+update = function(local_path, file_name)
+  if file_name == nil and type(local_path) == "table" then
+    update_legacy(local_path)
+  else
+    return pcall(check_file, local_path .. file_name, github_url .. file_name)
+  end
+end
+
+update_legacy = function(files)
   local success, result = pcall(function()
-    
-    local function check(local_filename, remote_url)
-      local f = io.open(local_filename, "rb")
-      if not f then return nil end
-      local local_script = f:read("*all"):gsub("\r\n", "\n")
-      f:close()
-
-      local response = {}
-      local _, dl_res = https.request {
-          url = remote_url,
-          sink = ltn12.sink.table(response)
-      }
-
-      if (dl_res ~= 200) then
-        ColourNote("red" , "black", "There was an error accessing " .. remote_url .. " - code: " .. dl_res)
-        return false
-      end
-
-      local remote_script = table.concat(response):gsub("\r\n", "\n")
-
-      if not remote_script then
-        return false
-      end
-
-      if local_script ~= remote_script then
-        os.remove(local_filename .. ".backup")
-        os.rename(local_filename, local_filename .. ".backup")
-        local new_file, res, res_code = io.open(local_filename, "wb")
-        if (f == nil) then
-          ColourNote("red" , "black", "There was an error updating: " .. res .. " - code: " .. res_code .. "! Manually update.")
-          os.rename(local_filename .. ".backup", local_filename)
-          return false
-        end
-
-        new_file:write(remote_script)
-        new_file:flush()
-        new_file:close()
-        return true
-      end
-
-      return false
-    end
-
     local is_updated = false
     for _, update_file in ipairs(files) do
-      if check(update_file["local_file"], update_file["remote_file"]) then
+      if check_file(update_file["local_file"], update_file["remote_file"]) then
         is_updated = true
       end
     end
@@ -63,4 +43,50 @@ function UPD.Update(files)
   end)
 end
 
-return UPD
+check_file = function(local_filename, remote_url)
+    local f = io.open(local_filename, "rb")
+    if not f then return nil end
+    local local_script = f:read("*all"):gsub("\r\n", "\n")
+    f:close()
+
+    local response = {}
+    local _, dl_res = https.request {
+        url = remote_url,
+        sink = ltn12.sink.table(response)
+    }
+
+    if (dl_res ~= 200) then
+      ColourNote("red" , "black", GetPluginName() .. ": [" .. dl_res .. "] There was an error updating from: " .. remote_url)
+      return false
+    end
+
+    local remote_script = table.concat(response):gsub("\r\n", "\n")
+
+    if not remote_script then
+      return false
+    end
+
+    if local_script ~= remote_script then
+      os.remove(local_filename .. ".backup")
+      os.rename(local_filename, local_filename .. ".backup")
+      local new_file, res, res_code = io.open(local_filename, "wb")
+      if (f == nil) then
+        ColourNote("red" , "black",  GetPluginName() .. ": [" .. res_code .. "] There was an error updating: " .. res .. "! Manually update the file and try again.")
+        os.rename(local_filename .. ".backup", local_filename)
+        return false
+      end
+
+      new_file:write(remote_script)
+      new_file:flush()
+      new_file:close()
+      return true
+    end
+
+    return false
+  end
+
+return {
+  version = 2,
+  Update = update,
+  UpdateAndRequire = updateAndRequire
+}
