2b96d37e31102073e43c596381e4deba1526ee91
notbilldavis <wdavis06@gmail.com>
2025-11-21 10:41:37 -0500

    added soothing nocture as a bad affect

--- lua/badaffects_miniwindow.lua
+++ lua/badaffects_miniwindow.lua
@@ -81,7 +81,8 @@ load = function()
       "poverty of ether", "immobilize", "damnation", "spider incubator", "entomb", "contagion", "faerie fire", "giant grasping hand", "existential horror", 
       "slurping proboscis", "harrow", "excommunicate", "boreworms disease", "irk", "nightmares", "hotfoot", "burning ember", "cavern sickness disease", 
       "mesmerize", "fatigue", "ash evocation", "evil eye", "hellfire surge", "latent charge", "static field", "weaken", "dead air", "light lungs disease", 
-      "earworm", "poverty of gems", "poverty of ithrilis", "poverty of dira", "poverty of gath", "lunar eclipse", "plodding fugue", "smoke evocation"
+      "earworm", "poverty of gems", "poverty of ithrilis", "poverty of dira", "poverty of gath", "lunar eclipse", "plodding fugue", "smoke evocation",
+      "soothing nocturne"
     }
     NEGATIVE_AFFECTS = {}
     for _, aff in ipairs(bad) do
